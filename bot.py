6bd5ef81d31abe48fc7f88a83da38562f6cb881d
CalvinBlunk <javamaster097@gmail.com>
2021-07-11 06:50:27 -0600

    Close to finished music

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import discord
 import random
 from discord.ext import commands
+import youtube_dl
 import os
 
-BALL_INSPECTOR_TOKEN = ''
 if os.path.isfile('home/pi/discord-token.txt'):
     txt_file = open('/home/pi/discord-token.txt', 'r')
     BALL_INSPECTOR_TOKEN = txt_file.read()
@@ -24,13 +24,68 @@ async def on_message(ctx):
         await ctx.channel.send('hey shartie wussup :smiling_face_with_3_hearts: :cold_face:')
 
 @bot.event
-async def on_join(user, ctx):
+async def on_join(member):
     rand = random.randint(1, 7)
-    file = discord.File('./gifs/inspection-{0}.gif'.format(rand))
-    await ctx.send(message=user.mention, file=file)
+    print(str(rand))
+    gif = discord.File('./gifs/inspection-{0}.gif'.format(rand))
+    await member.channel.send('welcome to hell {0}'.format(user.mention))
+    await member.channel.send(file=gif)
 
 @bot.command()
-async def test(ctx, *, arg):
-    await ctx.channel.send(arg)
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    await channel.connect()
+    await ctx.channel.send('joined **{0}** successfully'.format(channel))
+
+@bot.command()
+async def play(ctx, url: str):
+    songIsThere = os.path.isfile('song.mp3')
+    try:
+        if songIsThere:
+            os.remove('song.mp3')
+    except PermissionError:
+        await ctx.send('wait for the music to finish or use the **stop** command')
+    await join(ctx)
+    ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir('./'):
+        if file.endswith('.mp3'):
+            os.rename('song.mp3')
+    voice.play(discord.FFmpegPCMAudio('song.mp3')
+
+@bot.command()
+async def pause(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send('no audio be playin')
+
+@bot.command()
+async def resume(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send('no audio to pause')
+
+@bot.command()
+async def stop(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+@bot.command()
+async def leave(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    await ctx.voice_client.disconnect()
 
 bot.run(BALL_INSPECTOR_TOKEN)
